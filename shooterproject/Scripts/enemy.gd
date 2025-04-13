5240d8f0ad50303c8382211e7c80a3be0031fb3a
RadicalGG <C24435482@myTUDublin.ie>
2025-04-13 18:37:44 +0100

    enemy fix

--- shooterproject/Scripts/enemy.gd
+++ shooterproject/Scripts/enemy.gd
@@ -6,9 +6,10 @@ func _ready():
 	pass
 	
 func _physics_process(delta):
-	var Player = get_parent().get_node("player")
+	var Player = get_parent().get_node("Player")
 	position += (Player.position - position)/50
 	look_at(Player.position)
+	
 	move_and_collide(motion)
 
 
