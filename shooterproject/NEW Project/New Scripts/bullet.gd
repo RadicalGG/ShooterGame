bcb0447c8143ef7ef9cb32f62b01fa246f06f982
RadicalGG <C24435482@myTUDublin.ie>
2025-05-08 17:54:47 +0100

    ff

--- shooterproject/NEW Project/New Scripts/bullet.gd
+++ shooterproject/NEW Project/New Scripts/bullet.gd
@@ -12,7 +12,7 @@ func _physics_process(delta: float)  -> void:
 
 func _on_timer_timeout() -> void:
 	queue_free()
-
+#lol
 
 func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	if body.is_in_group("enemies"):
